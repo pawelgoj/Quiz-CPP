47812d9042897300f869861b1e543ca06df548fe
Pawel <71873238+pawelgoj@users.noreply.github.com>
2022-10-10 23:33:00 +0200

    Add check answer functionality

--- src/main.cpp
+++ src/main.cpp
@@ -8,26 +8,30 @@
 #include <wx/wx.h>
 #endif
 
-using std::string;
-using std::cout;
 using std::cin;
-using std::vector;
+using std::cout;
 using std::endl;
+using std::string;
+using std::to_string;
+using std::vector;
 
 class CLI
 {
-    private: 
+private:
     string input;
     QuestionList question_list;
 
-    public: CLI() {
+public:
+    CLI()
+    {
         this->input = "";
         this->question_list = QuestionList("Answers.json");
     };
 
-    void Run() {
-        
-        enum UserState 
+    void Run()
+    {
+
+        enum UserState
         {
             Menu,
             Question,
@@ -35,40 +39,46 @@ class CLI
         };
 
         UserState userState = Menu;
-        while(true) 
+        while (true)
         {
             if (input == "E" && Menu == userState)
             {
                 showExitMessage();
                 break;
             }
-            else if (input == "A" && (Menu == userState || Question == userState)) 
+            else if (input == "A" && (Menu == userState || Question == userState))
             {
                 userState = Question;
                 QuestionList &g = this->question_list;
                 int points = 0;
 
                 auto question_list_v = prepareQuestions(g, 5);
-                int length  = question_list_v.size();
+                int length = question_list_v.size();
 
-                for (int i = 0; i < length; i++) {
+                for (int i = 0; i < length; i++)
+                {
                     system("cls");
                     question_list_v[i].changeOrderOfAnswers();
                     showQuestion(input, question_list_v[i]);
 
-                    if (input == "E" && Question == userState) {
+                    if (input == "E" && Question == userState)
+                    {
                         break;
-                        
-                    } else {
+                    }
+                    else
+                    {
                         string character;
-                        //convert char to string
-                        character.push_back(question_list_v[i].GetCorrectAnswer().GetLetter()); 
+                        // convert char to string
+                        character.push_back(question_list_v[i].GetCorrectAnswer().GetLetter());
 
-                        if (character == input) {
+                        if (character == input)
+                        {
                             cout << "Correct answer!!!" << endl;
                             Sleep(2000);
                             points++;
-                        } else {
+                        }
+                        else
+                        {
                             cout << "Incorrect answer!!!" << endl;
                             Sleep(2000);
                         }
@@ -78,50 +88,62 @@ class CLI
                 userState = Menu;
                 showSummary(input, points, length);
             }
-            else if ((Menu == userState && input == "")
-                || (Question == userState && input == "E")) 
+            else if ((Menu == userState && input == "") || (Question == userState && input == "E"))
             {
-                system("cls"); //clear screen
+                system("cls"); // clear screen
                 input = showMenu(input);
                 userState = Menu;
             }
-            else if (Menu == userState) 
+            else if (Menu == userState)
             {
-                system("cls"); //clear screen
-                input = showWrongInputMessageInMenu(input); 
-            } else {
+                system("cls"); // clear screen
+                input = showWrongInputMessageInMenu(input);
+            }
+            else
+            {
                 throw("Something went wrong!!!");
             }
         }
     }
 
-    private: static void showQuestion(string &input, Question &question) {
-        
-        cout << "C++ Millionaires \n" << endl;
+private:
+    static void showQuestion(string &input, Question &question)
+    {
+
+        cout << "C++ Millionaires \n"
+             << endl;
         cout << question.GetQuestion() << endl;
 
-        Answer * answers  = question.GetAnswers();
+        Answer *answers = question.GetAnswers();
         char chars[4] = {'A', 'B', 'C', 'D'};
-        char ch; 
-        for (int i = 0; i < 4; i++) {
-            while(true) {
+        char ch;
+        for (int i = 0; i < 4; i++)
+        {
+            while (true)
+            {
                 ch = answers->GetLetter();
-                if (chars[i] == ch) {
+                if (chars[i] == ch)
+                {
                     cout << ch << " - " << answers->GetAnswer() << endl;
-                    answers  = question.GetAnswers();
+                    answers = question.GetAnswers();
                     break;
-                } else {
+                }
+                else
+                {
                     answers++;
                 }
             }
-        } 
+        }
         cout << "Chose answer 'A, B, C or D' " << endl;
         cout << "E - return to menu" << endl;
         cin >> input;
     }
 
-    private: static void showSummary(string &input, int &points, int &max_points) {
-        cout << "C++ Millionaires \n" << endl;
+private:
+    static void showSummary(string &input, int &points, int &max_points)
+    {
+        cout << "C++ Millionaires \n"
+             << endl;
         cout << "Your points is: " << points << "/" << max_points << endl;
         cout << "A - play again" << endl;
         cout << "E - exit" << endl;
@@ -129,32 +151,39 @@ class CLI
         cin >> input;
     }
 
-    private: static string showMenu(string input) {
-        cout << "C++ Millionaires \n" << endl;
+private:
+    static string showMenu(string input)
+    {
+        cout << "C++ Millionaires \n"
+             << endl;
         cout << "What do you wont to do?" << endl;
         cout << "A - draw a question" << endl;
         cout << "E - exit" << endl;
         cin >> input;
         return input;
     }
-    private: static void showExitMessage() 
+
+private:
+    static void showExitMessage()
     {
         cout << "Goodbye :)" << endl;
-    } 
-    private: static string showWrongInputMessageInMenu(string input) 
-    {   
+    }
+
+private:
+    static string showWrongInputMessageInMenu(string input)
+    {
         cout << "Insert 'A' or 'E'" << endl;
         cin >> input;
         return input;
     }
 
-    private: static vector<Question> prepareQuestions(QuestionList &question_list, int questionNumber)
+private:
+    static vector<Question> prepareQuestions(QuestionList &question_list, int questionNumber)
     {
         question_list.changeQuestionsOrder();
         vector<Question> list_of_questions = question_list.getQuestions(questionNumber);
         return list_of_questions;
     }
- 
 };
 
 class cApp : public wxApp
@@ -168,7 +197,7 @@ public:
 };
 
 /* Old cmd app.
-int main() 
+int main()
 {
 
     CLI cli;
--- src/main2.cpp
+++ src/main2.cpp
@@ -1,20 +1,27 @@
 // For compilers that support precompilation, includes "wx/wx.h".
 #include "questions.h"
 #include <cstdlib>
+#include <string>
+#include <wx/utils.h>
+#include <mutex>
+#include <chrono> //e.g. sleep function
+#include <thread>
 
 #include <wx/wxprec.h>
 #ifndef WX_PRECOMP
 #include <wx/wx.h>
 #endif
 
-using std::string;
 using std::cout;
 using std::endl;
+using std::string;
+using std::thread;
+using std::vector;
 
 class MyApp : public wxApp
 {
 public:
-    //function on initialization  
+    // function on initialization
     virtual bool OnInit();
 };
 class MyFrame : public wxFrame
@@ -29,11 +36,14 @@ public:
     wxButton *buttonB = nullptr;
     wxButton *buttonC = nullptr;
     wxButton *buttonD = nullptr;
-    wxStaticText *m_txt1 = nullptr; 
+    wxStaticText *m_txt1 = nullptr;
     wxGridSizer *grid = nullptr;
     wxColor inCorrectColour = wxColour(220, 20, 60, 0);
     wxColor correctColour = wxColour(65280);
-
+    u_int timeToShowResponseToUser = 1000;
+    u_int currentQuestion = 0;
+    u_int buttonIDList[4] = {31, 32, 33, 34};
+    bool buttonClicked = false;
 
 private:
     void OnHello(wxCommandEvent &event);
@@ -42,7 +52,13 @@ private:
     void OnCorrectButtonClicked(wxCommandEvent &event);
     void OnInCorrectButtonClicked(wxCommandEvent &event);
     void OnButtonStartClicked(wxCommandEvent &event);
+    void WaitToShowResponseToUserThread();
+    std::mutex dataMutex;
+    std::thread backgroundThread;
     wxDECLARE_EVENT_TABLE();
+
+public:
+    void prepareQuestionAndAnswersButtons(Question question, u_int *buttonIDList);
 };
 
 enum
@@ -50,20 +66,24 @@ enum
     ID_Hello = 1
 };
 
-//event table macros 
+wxIMPLEMENT_APP(MyApp);
+// wxIMPLEMENT_APP(MyApp); implement dynamic constructor for MyApp
+
+// event table macros
 wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
     EVT_BUTTON(20, MyFrame::OnButtonStartClicked)
-    EVT_BUTTON(31, MyFrame::OnCorrectButtonClicked)
-    EVT_BUTTON(32, MyFrame::OnInCorrectButtonClicked)
-    EVT_MENU(ID_Hello, MyFrame::OnHello)
-        EVT_MENU(wxID_EXIT, MyFrame::OnExit)
-    EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
-    
-wxEND_EVENT_TABLE()
-                    wxIMPLEMENT_APP(MyApp);
+        EVT_BUTTON(31, MyFrame::OnCorrectButtonClicked)
+            EVT_BUTTON(32, MyFrame::OnInCorrectButtonClicked)
+                EVT_BUTTON(33, MyFrame::OnInCorrectButtonClicked)
+                    EVT_BUTTON(34, MyFrame::OnInCorrectButtonClicked)
+                        EVT_MENU(ID_Hello, MyFrame::OnHello)
+                            EVT_MENU(wxID_EXIT, MyFrame::OnExit)
+                                EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
 
-//function on initialization of app
-bool MyApp::OnInit()
+                                    wxEND_EVENT_TABLE()
+
+    // function on initialization of app
+    bool MyApp::OnInit()
 {
     MyFrame *frame = new MyFrame("Millionaires C++", wxPoint(50, 50), wxSize(450, 340));
     frame->Show(true);
@@ -72,7 +92,7 @@ bool MyApp::OnInit()
 MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
     : wxFrame(NULL, wxID_ANY, title, pos, size)
 {
-    //Crate menu 
+    // Crate menu
     wxMenu *menuFile = new wxMenu;
     menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
                      "Help string shown in status bar for this menu item");
@@ -86,36 +106,34 @@ MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
     SetMenuBar(menuBar);
     CreateStatusBar();
     SetStatusText("Millionaires C++");
-    //create grid for widgets 
+    // create grid for widgets
     grid = new wxGridSizer(2, 1, 0, 0);
 
-    //Crate text filed 
+    // Crate text filed
 
-    //To achieve text vertically centered wxPanel is used.
-    wxPanel* pseudoText = new wxPanel(this, wxID_ANY, wxDefaultPosition,
-    wxDefaultSize, wxBORDER_THEME|wxTAB_TRAVERSAL);
-    m_txt1 = new wxStaticText(pseudoText, 21, "Millionaires C++", wxDefaultPosition, 
-    wxDefaultSize, wxALIGN_CENTRE_HORIZONTAL);
+    // To achieve text vertically centered wxPanel is used.
+    wxPanel *pseudoText = new wxPanel(this, wxID_ANY, wxDefaultPosition,
+                                      wxDefaultSize, wxBORDER_THEME | wxTAB_TRAVERSAL);
+    m_txt1 = new wxStaticText(pseudoText, 21, "Millionaires C++", wxDefaultPosition,
+                              wxDefaultSize, wxALIGN_CENTRE_HORIZONTAL);
     m_txt1->SetBackgroundColour(wxColour(240, 248, 255, 0));
     pseudoText->SetBackgroundColour(m_txt1->GetBackgroundColour());
 
-    wxBoxSizer* szr = new wxBoxSizer(wxVERTICAL);
+    wxBoxSizer *szr = new wxBoxSizer(wxVERTICAL);
     szr->AddStretchSpacer();
     szr->Add(m_txt1, wxSizerFlags().Expand());
     szr->AddStretchSpacer();
     pseudoText->SetSizer(szr);
-    
-    grid->Add(pseudoText, 1, wxEXPAND | wxALL );
-    //Create button 
+
+    grid->Add(pseudoText, 1, wxEXPAND | wxALL);
+    // Create button
     m_btn1 = new wxButton(this, 20, "Start game");
-    //Add button to grid, expand button to fill all space of cal 
-    grid->Add(m_btn1, 1, wxEXPAND | wxALL );
+    // Add button to grid, expand button to fill all space of cal
+    grid->Add(m_btn1, 1, wxEXPAND | wxALL);
     this->SetSizer(grid);
 
     m_txt1->SetFont(wxFontInfo(12).Bold());
     grid->Layout();
-    
-    
 }
 void MyFrame::OnExit(wxCommandEvent &event)
 {
@@ -133,59 +151,132 @@ void MyFrame::OnHello(wxCommandEvent &event)
 
 void MyFrame::OnButtonStartClicked(wxCommandEvent &event)
 {
-    //Add String to list from text box.
-    //draw a question 
-    //prepare answers 
+    // Add String to list from text box.
+    // draw a question
+    // prepare answers
 
     QuestionList questionList = QuestionList(".\\Answers.json");
-    
+
     questionList.changeQuestionsOrder();
 
-    this->listOfQuestions = questionList.getQuestions(7); //will be 10 questions 
+    this->listOfQuestions = questionList.getQuestions(7); // will be 10 questions
 
+    Question question = listOfQuestions[0];
+
+    MyFrame::prepareQuestionAndAnswersButtons(question, this->buttonIDList);
+    // finish event
+    this->currentQuestion = this->currentQuestion + 1;
+}
+void MyFrame::prepareQuestionAndAnswersButtons(Question question, u_int *buttonIDList)
+{
+    if (this->buttonClicked)
+    {
+        this->buttonClicked = false;
+        this->backgroundThread.join();
+    }
+    auto answers = question.GetAnswers();
     grid->Clear(true);
     grid->SetCols(1);
     grid->SetRows(2);
-
-    Question question = listOfQuestions[0];
     wxGridSizer *inGrid = new wxGridSizer(2, 2, 0, 0);
-    buttonA = new wxButton(this, 31, "A", wxDefaultPosition, 
-    wxDefaultSize);
+    string answerChar = "A-";
+    buttonA = new wxButton(this, *buttonIDList, answerChar.append(answers[0].GetAnswer()), wxDefaultPosition,
+                           wxDefaultSize);
     inGrid->Add(buttonA, 1, wxEXPAND | wxALL);
-    buttonB = new wxButton(this, 32, "B", wxDefaultPosition, 
-    wxDefaultSize);
+    answerChar = "B-";
+    buttonB = new wxButton(this, *(buttonIDList + 1), answerChar.append(answers[1].GetAnswer()), wxDefaultPosition,
+                           wxDefaultSize);
     inGrid->Add(buttonB, 1, wxEXPAND | wxALL);
-    buttonC = new wxButton(this, 33, "C", wxDefaultPosition, 
-    wxDefaultSize);
+    answerChar = "C-";
+    buttonC = new wxButton(this, *(buttonIDList + 2), answerChar.append(answers[2].GetAnswer()), wxDefaultPosition,
+                           wxDefaultSize);
     inGrid->Add(buttonC, 1, wxEXPAND | wxALL);
-    buttonD = new wxButton(this, 34, "D", wxDefaultPosition, 
-    wxDefaultSize);
+    answerChar = "D-";
+    buttonD = new wxButton(this, *(buttonIDList + 3), answerChar.append(answers[3].GetAnswer()), wxDefaultPosition,
+                           wxDefaultSize);
     inGrid->Add(buttonD, 1, wxEXPAND | wxALL);
 
     wxString questionString = question.GetQuestion();
-    m_txt1 = new wxStaticText(this, 21, questionString, wxDefaultPosition, 
-    wxDefaultSize, wxALIGN_CENTRE_HORIZONTAL);
+    // The flag wxST_NO_AUTORESIZE is must be used with wxALIGN_CENTRE_HORIZONTAL then
+    // SetLabel work correctly. The size of wxStaticText not resise while Method SetLabel is used.
+    m_txt1 = new wxStaticText(this, 21, questionString, wxDefaultPosition,
+                              wxDefaultSize, wxALIGN_CENTRE_HORIZONTAL | wxST_NO_AUTORESIZE);
     m_txt1->SetBackgroundColour(wxColour(240, 248, 255, 0));
+    m_txt1->SetForegroundColour(wxColour(0, 0, 0, 0));
     m_txt1->SetFont(wxFontInfo(12).Bold());
 
     grid->Add(m_txt1, 1, wxEXPAND | wxALL);
     grid->Add(inGrid, 1, wxEXPAND | wxALL);
-    //Force the layout of children anew 
+    // Force the layout of children anew
     grid->Layout();
-    //finish event 
-    event.Skip();
 }
 
 void MyFrame::OnCorrectButtonClicked(wxCommandEvent &event)
 {
-    buttonA->SetBackgroundColour(this->correctColour); 
-    m_txt1->SetLabel("Correct answer!!!!");
-    grid->Layout();
+    if (this->buttonClicked)
+    {
+        event.Skip();
+    }
+    else
+    {
+        this->buttonClicked = true;
+        wxBeginBusyCursor();
+        m_txt1->SetLabel("Correct answer!!!!");
+        this->buttonA->SetBackgroundColour(this->correctColour);
+        // this->buttonA->Disable();
+        this->buttonA->Enable(false);
+        this->buttonA->Update();
+        grid->Layout();
+        // Czeka na inne wiadomości z gui
+        this->backgroundThread = std::thread{&MyFrame::WaitToShowResponseToUserThread, this};
+        // wait 1s
+    }
+}
+
+void MyFrame::WaitToShowResponseToUserThread()
+{ // wait in msdad
+    wxThread::Sleep(2000);
+    wxEndBusyCursor();
+
+    wxGetApp().CallAfter([this]()
+                         {
+    Question question = listOfQuestions[this->currentQuestion];
+    this->currentQuestion = this->currentQuestion + 1;
+    MyFrame::prepareQuestionAndAnswersButtons(question, this->buttonIDList);
+    this->buttonClicked = false; });
 }
 
 void MyFrame::OnInCorrectButtonClicked(wxCommandEvent &event)
 {
-    buttonB->SetBackgroundColour(this->inCorrectColour);
-    buttonA->SetBackgroundColour(this->correctColour);
+    if (this->buttonClicked)
+    {
+        event.Skip();
+    }
+    else
+    {
+        this->buttonClicked = true;
+        wxBeginBusyCursor();
+        wxObject *obj = event.GetEventObject();
+        ((wxButton *)obj)->SetBackgroundColour(this->inCorrectColour);
+        buttonA->SetBackgroundColour(this->correctColour);
+        m_txt1->SetLabel("Incorrect answer!!!!");
+        ((wxButton *)obj)->Enable(false);
+        ((wxButton *)obj)->Update();
+        grid->Layout();
+        // wait 1s
+        this->backgroundThread = std::thread{&MyFrame::WaitToShowResponseToUserThread, this};
+    }
 }
-
+/*
+class MyThread : public wxThread
+{
+public:
+    MyThread(MyFrame *handler)
+        : wxThread(wxTHREAD_DETACHED)
+        { m_pHandler = handler }
+    ~MyThread();
+protected:
+    virtual ExitCode Entry();
+    MyFrame *m_pHandler;
+};
+*/
