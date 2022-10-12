ad4e36b0b07d84670d2a3cea7f7038cb89c99176
Pawel <71873238+pawelgoj@users.noreply.github.com>
2022-10-13 00:56:19 +0200

    Refactor

--- src/questions.h
+++ src/questions.h
@@ -2,34 +2,35 @@
 #define PYTANIE_H
 
 #include <iostream>
-#include "json.hpp"
+#include "../libs/nlohmann_json/json.hpp"
 #include <fstream>
 #include <vector>
 #include <cstdlib>
 #include <time.h>
 
 /*
-The question object is container for the string of the question , correct and incorrect answers. 
+The question object is container for the string of the question , correct and incorrect answers.
 Also the correct answer letter is stored.
 */
 
-using std::string;
-using std::cout;
 using std::cin;
+using std::cout;
+using std::string;
 using std::vector;
 using json = nlohmann::json;
+using std::copy;
 using std::endl;
 using std::ifstream;
-using std::copy;
 
-class Answer {
-    public:
-        string answer;
-        bool isCorrect;
-        char letter;
-        Answer();
-        
-        Answer(string x, bool y, char letter);
+class Answer
+{
+public:
+    string answer;
+    bool isCorrect;
+    char letter;
+    Answer();
+
+    Answer(string x, bool y, char letter);
 
     string GetAnswer();
 
@@ -38,12 +39,14 @@ class Answer {
     char GetLetter();
 };
 
-class Question {
-    private:
+class Question
+{
+private:
     int id;
-    string question; 
+    string question;
     Answer answers[4];
-    public: 
+
+public:
     Question();
 
     int GetId();
@@ -58,22 +61,19 @@ class Question {
 
     void changeOrderOfAnswers();
 
-    Answer * GetAnswers();
+    Answer *GetAnswers();
 };
 
-class QuestionList {
-    public:
-        vector<Question> questionsList;
-        string fileName;
-    QuestionList(string fileName="Answers.json");
+class QuestionList
+{
+public:
+    vector<Question> questionsList;
+    string fileName;
+    QuestionList(string fileName = "Answers.json");
 
     void changeQuestionsOrder();
 
     vector<Question> getQuestions(int questionNumber);
-
 };
 
 #endif
-
-
-
